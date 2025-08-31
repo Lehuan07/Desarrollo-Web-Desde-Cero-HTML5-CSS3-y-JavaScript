ebb248d459120a78372ac16eb9df8198a35fa442
Nahuel C. <nahuelncejas@gmail.com>
2025-08-31 13:19:24 -0300

    Nav lateral para mobile echo

--- 14 PRACTICA FINAL Web para pizzeria/assets/js/app.js
+++ 14 PRACTICA FINAL Web para pizzeria/assets/js/app.js
@@ -3,10 +3,15 @@
 
 const bars = document.querySelector('.fa-bars');
 const nav  = document.querySelector('.layout__nav');
+const arrow = document.querySelector(".fa-angle-right")
 
 bars.addEventListener('click', () => {
-    nav.classList.toggle('nav__active');
-
+    nav.classList.add('layout__nav-active');
     bars.style.display = "none";
     
 });
+
+arrow.addEventListener('click', () =>{
+    nav.classList.remove('layout__nav-active');
+    bars.style.display = "block"
+})
