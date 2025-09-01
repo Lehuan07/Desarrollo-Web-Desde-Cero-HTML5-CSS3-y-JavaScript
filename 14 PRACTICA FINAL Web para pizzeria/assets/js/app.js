45f4926233ff70539d405cf4b3ddee1f31867e33
Nahuel C. <nahuelncejas@gmail.com>
2025-08-31 21:09:35 -0300

    Haciendo el slider

--- 14 PRACTICA FINAL Web para pizzeria/assets/js/app.js
+++ 14 PRACTICA FINAL Web para pizzeria/assets/js/app.js
@@ -3,25 +3,28 @@
 
 const bars = document.querySelector('.fa-bars');
 const nav  = document.querySelector('.header__principal');
-const arrow = document.querySelector(".fa-angle-right")
+const arrow = document.querySelector(".move__right--mobile-hiden")
 const enlaces = document.querySelector(".principal__left")
 
 bars.addEventListener('click', () => {
     bars.style.display = "none";
-    arrow.classList.remove('fa-angle-right-hiden')
+    arrow.classList.remove('move__right--mobile-hiden')
+    arrow.classList.add('move__right--mobile')
     nav.classList.add('header__principal-active')
 });
 
 arrow.addEventListener('click', () =>{
-    arrow.classList.add('fa-angle-right-hiden')
+    arrow.classList.remove('move__right--mobile')
+    arrow.classList.add('move__right--mobile-hiden')
     bars.style.display = "block"
     nav.classList.remove('header__principal-active')
 })
 
 enlaces.addEventListener('click', ()=>{
     setTimeout(()=>{
+        arrow.classList.remove('move__right--mobile')
         nav.classList.remove('header__principal-active')
-        arrow.classList.add('fa-angle-right-hiden')
+        arrow.classList.add('move__right--mobile-hiden')
         bars.style.display = "block"
 
     }, 500)
